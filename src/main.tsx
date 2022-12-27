bee9436abd6dd797f08ef50471135d8ebdce9a59
Jaruwat Thongchana <jaruwat.works@gmail.com>
2022-12-27 21:05:02 +0700

    fix: fotmat script in root path

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ClickToComponent } from "click-to-react-component";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Error from "./pages/error_page";
-import "./assets/css/index.css";
-import ResponsiveAppBar from "./components/appbar";
-import LandingPage from "./pages/landing_page";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClickToComponent } from 'click-to-react-component'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './pages/error_page'
+import './assets/css/index.css'
+import ResponsiveAppBar from './components/appbar'
+import LandingPage from './pages/landing_page'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <ResponsiveAppBar />,
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <LandingPage />,
       },
     ],
   },
-]);
+])
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <ClickToComponent />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
